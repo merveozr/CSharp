dd66266f8273e174dade9017f44a5f4862ed7a92
Merve Ozer <merveozrr.01@gmail.com>
2025-12-12 12:10:53 +0300

    Cerver ve client olusturuldu.

---
+++ Client/Client/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_IP = new System.Windows.Forms.TextBox();
+            this.textBox_Port = new System.Windows.Forms.TextBox();
+            this.textBox_Message = new System.Windows.Forms.TextBox();
+            this.clientLogs = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button_connect = new System.Windows.Forms.Button();
+            this.button_send = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_IP
+            // 
+            this.textBox_IP.Location = new System.Drawing.Point(66, 36);
+            this.textBox_IP.Name = "textBox_IP";
+            this.textBox_IP.Size = new System.Drawing.Size(173, 22);
+            this.textBox_IP.TabIndex = 0;
+            // 
+            // textBox_Port
+            // 
+            this.textBox_Port.Location = new System.Drawing.Point(66, 70);
+            this.textBox_Port.Name = "textBox_Port";
+            this.textBox_Port.Size = new System.Drawing.Size(173, 22);
+            this.textBox_Port.TabIndex = 1;
+            // 
+            // textBox_Message
+            // 
+            this.textBox_Message.Enabled = false;
+            this.textBox_Message.Location = new System.Drawing.Point(99, 248);
+            this.textBox_Message.Name = "textBox_Message";
+            this.textBox_Message.Size = new System.Drawing.Size(227, 22);
+            this.textBox_Message.TabIndex = 2;
+            // 
+            // clientLogs
+            // 
+            this.clientLogs.Location = new System.Drawing.Point(256, 33);
+            this.clientLogs.Name = "clientLogs";
+            this.clientLogs.Size = new System.Drawing.Size(228, 198);
+            this.clientLogs.TabIndex = 3;
+            this.clientLogs.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Port:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 250);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Message:";
+            // 
+            // button_connect
+            // 
+            this.button_connect.Location = new System.Drawing.Point(99, 114);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(90, 26);
+            this.button_connect.TabIndex = 7;
+            this.button_connect.Text = "Connect";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.button_connect_Click);
+            // 
+            // button_send
+            // 
+            this.button_send.Enabled = false;
+            this.button_send.Location = new System.Drawing.Point(346, 248);
+            this.button_send.Name = "button_send";
+            this.button_send.Size = new System.Drawing.Size(90, 26);
+            this.button_send.TabIndex = 8;
+            this.button_send.Text = "Send";
+            this.button_send.UseVisualStyleBackColor = true;
+            this.button_send.Click += new System.EventHandler(this.button_send_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 300);
+            this.Controls.Add(this.button_send);
+            this.Controls.Add(this.button_connect);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.clientLogs);
+            this.Controls.Add(this.textBox_Message);
+            this.Controls.Add(this.textBox_Port);
+            this.Controls.Add(this.textBox_IP);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_IP;
+        private System.Windows.Forms.TextBox textBox_Port;
+        private System.Windows.Forms.TextBox textBox_Message;
+        private System.Windows.Forms.RichTextBox clientLogs;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button_connect;
+        private System.Windows.Forms.Button button_send;
+    }
+}
+
---
+++ Client/Client/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        bool connected = false;
+        bool terminated = true;
+        Socket clientSocket;
+
+        public Form1()
+        {
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void button_connect_Click(object sender, EventArgs e)
+        {
+            clientSocket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            string IP = textBox_IP.Text;
+            int portNum;
+            if(Int32.TryParse(textBox_Port.Text, out portNum))
+            {
+                try
+                {
+                    clientSocket.Connect(IP, portNum);
+                    button_send.Enabled = true;
+                    textBox_Message.Enabled = true;
+                    button_connect.Enabled = false;
+                    connected = true;
+                    clientLogs.AppendText("Connected to the server!\n");
+                }
+                catch
+                {
+                    clientLogs.AppendText("Could not connect to the server!\n");
+                }
+            }
+
+            else
+            {
+                clientLogs.AppendText("Check the port (must integer) \n");
+            }
+
+        }
+
+        private void button_send_Click(object sender, EventArgs e)
+        {
+            string message = textBox_Message.Text;
+            if(message != "" && message.Length <=64)
+            {
+                try
+                {
+                    Byte[] buffer = Encoding.Default.GetBytes(message);
+                    clientSocket.Send(buffer);
+                }
+                catch
+                {
+                    clientLogs.AppendText("A mistake happened while trying to send");
+                }
+
+            }
+
+        }
+        private void Form1_FormClosing(object sender, System.ComponentModel.CancelEventArgs e) 
+        {
+            connected = false;
+            terminated = true;
+            Environment.Exit(0);
+        }
+    }
+}
